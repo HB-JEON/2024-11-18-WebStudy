f0d62e8ae0f99313821f678676be50bd0df30fea
sist-106 <sist-106@DESKTOP-RE7C273>
2025-03-06 10:10:14 +0900

    2025-03-06 git

---
+++ JSPFrontProject_7/src/main/java/com/sist/vo/SalGradeVO.java
@@ -0,0 +1,29 @@
+package com.sist.vo;
+
+public class SalGradeVO {
+	private int grade,hisal,losal;
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	public int getHisal() {
+		return hisal;
+	}
+
+	public void setHisal(int hisal) {
+		this.hisal = hisal;
+	}
+
+	public int getLosal() {
+		return losal;
+	}
+
+	public void setLosal(int losal) {
+		this.losal = losal;
+	}
+}
